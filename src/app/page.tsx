0b96820a4857cc78255b797391c8c0be0399c3de
Bart Tyrpien <bartlomein123@gmail.com>
2024-08-08 08:36:43 -0400

    remove unused

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import NFTCollectionsDashboard from "@/components/NFTCollectionsDashboard/NFTCollectionsDashboard";
-
-export default function Home() {
-  return (
-    <main>
-      <NFTCollectionsDashboard limit={12} />
-    </main>
-  );
-}
